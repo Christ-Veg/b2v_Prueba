6bbeaf61d3e0d0a05fe10ab955f41197e4d506cd
Emanuel Vega <christian990116@gmail.com>
2022-03-11 01:08:48 -0600

    Se corrige la creacion de usuario con rol por default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
-            $table->integer('rol');
+            $table->integer('rol')->default(1);
         });
     }
 
